928227c3f3b683daa42bcc8dbc2ef2610e249f64
Saulius Petreikis <saulius.petreikis@nfq.lt>
2023-07-31 10:21:57 +0300

    finishing the streams excercise

--- streams/streams.js
+++ streams/streams.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
+const path = require('path');
 const through = require('through2');
 
-/* Create a read stream here
-const readPoemStream = ???
-*/
+// Create a read stream here
+const readPoemStream = fs.createReadStream(path.join(__dirname, 'on-joy-and-sorrow-emoji.txt'));
 
-/* Create a write stream here
-const writePoemStream =
-*/
+readPoemStream.on('data', (chunk) => { 
+    console.log(chunk.toString().replace(':)', 'joy').replace(':(', 'sorrow')); 
+});
+
+// Create a write stream here
+const writePoemStream = fs.createWriteStream(path.join(__dirname, 'on-joy-and-sorrow-fixed.txt'));
+
+const transformStream = through(function(chunk, encoding, callback) {
+    this.push(chunk.toString().replace(':)', 'joy').replace(':(', 'sorrow'));
+
+    callback();
+});
+
+readPoemStream.pipe(transformStream).pipe(writePoemStream);
 
 /* EXTENSION: Create a transform stream (modify the read stream before piping to write stream)
 const transformStream = ???
